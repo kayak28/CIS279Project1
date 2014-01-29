76e7fda462e722acd8d94a89476c987c191af7b7
kayak28 <kaya_ota@msn.com>
2014-01-28 22:18:21 -0800

    add include time.h

--- retruned/Testmain.cpp
+++ retruned/Testmain.cpp
@@ -5,12 +5,13 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-
+#include <time.h>
 using namespace std;
 
 
 int main()
 {
+	srand(time(NULL));
 	printf("Hello!\n");
 	EightSideDie die1;
 	printf("hi");
